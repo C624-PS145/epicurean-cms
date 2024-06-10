f12992d027439751b3c2f3e2c7f8204934fbcb02
YohanpermanaRepository <yohandhikristantara99@gmail.com>
2024-06-11 02:09:50 +0700

    konfigurasi shadowdom js login

---
+++ javascript/login.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('loginForm').addEventListener('submit', function(event) {
+        event.preventDefault();
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+
+        fetch('http://localhost:3000/api/authlogin', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username, password })
+        })
+        .then(response => {
+            if (response.status === 200) {
+                // Simpan status login ke localStorage
+                localStorage.setItem('isLoggedIn', true);
+                window.location.href = './../pages/project.html'; // Arahkan ke halaman proyek setelah login berhasil
+            } else {
+                document.getElementById('errorMsg').style.display = 'block';
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    });
+
+    // Periksa status login saat halaman dimuat
+    if (localStorage.getItem('isLoggedIn')) {
+        // Jika pengguna sudah login, arahkan ke halaman proyek
+        window.location.href = './../pages/project.html';
+    }
+});
